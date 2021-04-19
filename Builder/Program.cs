b74ff50e88bf3c8b940c7ad3a14d0039f2316a73
AfraAllahyani <Afra.Allahyani@tuwaiq.edu.sa>
2021-04-19 04:26:24 +0300

    first commit

---
+++ Builder/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Builder
+{
+    class Program
+    {
+        class grbBuilder
+        {
+            private string rgbFormatter;
+            public grbBuilder()
+            {
+                this.rgbFormatter = "";
+            }
+            public grbBuilder beginTag()
+            {
+                this.rgbFormatter += "color: rgb(";
+                return this;
+            }
+            public grbBuilder setColors(int red, int green, int blue)
+            {
+                this.rgbFormatter += red + "," + green + "," + blue;
+                return this;
+            }
+            public grbBuilder endTag()
+            {
+                this.rgbFormatter += ")";
+                return this;
+            }
+            public string getFormat()
+            {
+                return this.rgbFormatter;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            grbBuilder obj = new grbBuilder();
+            string grbFormat =
+                obj.beginTag()
+                .setColors(255, -10, 0)
+                .endTag()
+                .getFormat();
+            Console.WriteLine(grbFormat);
+
+        }
+    }
+}
